e986a44938bd436697d2092eae79f5ef1a8ef008
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-03-06 13:55:08 +0900

    Submit challenge for grading

--- 01_introduction_to_software_development_in_python/02_challenge/main.py
+++ 01_introduction_to_software_development_in_python/02_challenge/main.py
@@ -3,71 +3,101 @@ import math
 
 # 2. 10と20を足し算した結果 (和) を求めてください
 # ここにコードを書いてください
-sum_result = 
+a = 10+20
+print(a)
+sum_result = 30
 
 # 3. 3.5と2を掛け算した結果 (積) を求めてください
 # ここにコードを書いてください
-product_result = 
+b = 3.5*2
+print(b)
+product_result = 7
 
 # 4. 10を3で割り算した結果 (商) を求めてください
 # ここにコードを書いてください
-division_result = 
+c = 10/3
+print(c)
+division_result = 3.3333333333333335
 
 # 5. 10を3で割り算した余りを求めてください
 # ここにコードを書いてください
-remainder_result = 
+d = 10%3
+print(d)
+remainder_result = 1
 
 # 6. 2の3乗を求めてください
 # ここにコードを書いてください
-power_result = 
+e = 2**3
+print(e)
+power_result = 8
 
 # 7. 25の平方根を求めてください
 # ヒント: math.sqrt() の使い方を調べることをおすすめします
 # ここにコードを書いてください
-sqrt_result = 
+f = math.sqrt(25)
+print(f)
+sqrt_result = 5.0
 
 # 8. -10の絶対値を求めてください
 # ヒント: abs() の使い方を調べることをおすすめします
 # ここにコードを書いてください
-absolute_value = 
+g = abs(-10)
+print(g)
+absolute_value = 10
 
 # 9. 10を3で整数除算 (切り捨て除算) した結果を求めてください
-# ここにコードを書いてください
-floor_division_result = 
+# ここにコードを書いてください10
+h= 10//3
+print(h)
+floor_division_result = 3
 
 # 10. 10、20、30の平均を求めてください
 # ここにコードを書いてください
-average_result = 
+j= (10+10+20+30)/4
+print(j)
+average_result = 17.5
 
 # 11. 華氏98.6度を摂氏に変換してください
 fahrenheit = 98.6
 # ここにコードを書いてください
-celsius = 
+k= (fahrenheit-32)*5/9
+print(k)
+celsius = 37
 
 # 12. 縦5、横3の長方形の周囲の長さを求めてください
 length = 5
 width = 3
 # ここにコードを書いてください
-perimeter = 
+l=length*2+width*2
+print(l)
+perimeter = 16
 
 # 13. 半径5の円の面積を求めてください
 # ヒント: math.piの使い方を調べることをおすすめします
 radius = 5
 # ここにコードを書いてください
-area_circle = 
+m=math.pi*radius**2
+print(m)
+area_circle = 78.53981633974483
 
 # 14. 時速100kmは秒速何メートルですか
 km_per_hour = 100
 # ここにコードを書いてください
-meters_per_second = 
+n= 100*1000/3600
+print(n)
+meters_per_second = 27.77777777777778
 
 # 15. 直角三角形があり、2つの辺の長さはそれぞれ3と4です。この三角形の斜辺の長さを求めてください
 side1 = 3
 side2 = 4
 # ここにコードを書いてください
-hypotenuse = 
+o=(side1**2+side2**2)**0.5
+print(o)
+hypotenuse = 5
 
 # 16. 半径5の球の体積を求めてください
 radius_sphere = 5
 # ここにコードを書いてください
-volume_sphere = 
+p=4/3*math.pi*radius_sphere**3
+print(p)
+volume_sphere = 523.5987755982989
