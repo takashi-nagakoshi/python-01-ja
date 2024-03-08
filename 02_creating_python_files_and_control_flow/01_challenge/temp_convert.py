3a3be97cde8c36fbaf2622d6c595ecb1572739e8
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-03-08 20:50:57 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,9 +1,21 @@
 def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = 0
+    temp = int(input("enter a number:"))
+    temp_unit=input("f , c")
+    x = temp
+    y = temp_unit
+    # f = 華氏、c = 摂氏
+    if (y == 'f'):
+        C = (x -32)*5/9
+        print(int(C))
+    elif (y =='c'):
+        F = (x * 9/5) + 32
+        print(int(F))
+    
+#     temp = 0
 
-    return temp
+#     return temp
 
 
-convert()
+# convert()
--- 02_creating_python_files_and_control_flow/02_challenge/factorial.py
+++ 02_creating_python_files_and_control_flow/02_challenge/factorial.py
@@ -1,12 +1,28 @@
 def compute_factorial():
     # ここにコードを書いてください
     # number変数を編集し、ユーザー入力として正の整数を受け取ります。整数に変換することを忘れないでください
-    number = 0
+    
+    number = int(input())
+    if number == 0:
+        return 1 
 
+    count = number
+    while(count > 0):
+        if count ==1:
+            break
+
+        number = number * (count - 1)
+        count = count - 1
     # result変数を編集し、最終的な計算結果を保存します
-    result = 0
+    result = number
+    print(result)
 
     return result
 
-
 compute_factorial()
+
+#for loop
+#range(5) = 0,1,2,3,4
+#range(1,5) = 1,2,3,4
+#number = number * oneNumber
+#linter
