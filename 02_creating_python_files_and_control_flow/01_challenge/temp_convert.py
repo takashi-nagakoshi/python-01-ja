fa8c196a54d10502c2628b69da499e1ad407c0d9
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-03-14 15:21:43 +0900

    Submit challenge for grading

--- 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
+++ 02_creating_python_files_and_control_flow/01_challenge/temp_convert.py
@@ -1,21 +1,30 @@
-def convert():
+# def convert():
     # ここにコードを書いてください
     # temp変数を編集し、ユーザー入力として温度を受け取ります。整数に変換することを忘れないでください
-    temp = int(input("enter a number:"))
-    temp_unit=input("f , c")
-    x = temp
-    y = temp_unit
+
+#入力値が華氏の場合
+def fahrenheit_to_celsius(fahrenheit):
+        return (fahrenheit - 32) * 5/9
+#入力値が摂氏の場合
+def celsius_to_fahrenheit(celsius):
+        return celsius * 9/5 + 32
+def convert():
+        temperature = float(input("温度を入力してください： "))
+        unit = input("温度の単位を入力してください（華氏または摂氏）： ")
+
+        if unit.lower() == '華氏':
+            celsius = fahrenheit_to_celsius(temperature)
+            print(f"摂氏 {celsius:.2f} 度です。")
+        elif unit.lower() == '摂氏':
+            fahrenheit = celsius_to_fahrenheit(temperature)
+            print(f"華氏 {fahrenheit:.2f} 度です。")
+        else:
+            print("無効な単位が入力されました。華氏または摂氏で入力してください。")
+if __name__ == "__main__":
+    convert()
     # f = 華氏、c = 摂氏
-    if (y == 'f'):
-        C = (x -32)*5/9
-        print(int(C))
-    elif (y =='c'):
-        F = (x * 9/5) + 32
-        print(int(F))
-    
 #     temp = 0
 
 #     return temp
-
 
 # convert()
--- 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
+++ 02_creating_python_files_and_control_flow/03_challenge/count_vowels.py
@@ -1,1 +1,12 @@
 # ここにコードを書いてください
+def count_vowels(input_string):
+    vowels = "aiueoAIUEO"
+    vowel_count = 0
+    for char in input_string:
+        if char in vowels:
+            vowel_count += 1
+    return vowel_count
+
+user_input = input("文字列を入力してください:")
+vowel_count = count_vowels(user_input)
+print("Number of vowels:",vowel_count)
