65968c265d5d2fff66b6ecaac0683a1f0f6a01bf
Takashi Nagakoshi <takashi.nagakoshi@woven-planet.global>
2024-03-18 22:05:15 +0900

    Submit challenge for grading

--- 04_basic_functions/01_challenge/calculator.py
+++ 04_basic_functions/01_challenge/calculator.py
@@ -1,14 +1,27 @@
 def add(a, b):
-    return 0
-
+    if isinstance(a,(int,float)) and isinstance(b, (int,float)):
+        return  a + b
+    else:
+        return "bad input"
 
 def subtract(a, b):
-    return 0
-
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a - b
+    else:
+        return "bad input"
 
 def multiply(a, b):
-    return 0
-
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        return a * b
+    else:
+        return "bad input"
 
 def divide(a, b):
-    return 0
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        if b != 0:
+            return a / b
+        else:
+            return "Cannot divide by zero"
+    else:
+        return "bad input"
+    
